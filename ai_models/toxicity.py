3b18374366d13f09f716c901ad610cbe7a396b5a
Priyansh Khare <priyanshkhare2005@gmail.com>
2025-09-05 17:34:37 +0530

    toxicity model to accept json

--- ai_models/toxicity.py
+++ ai_models/toxicity.py
@@ -1,6 +1,10 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
 import requests
 import json
+from typing import List
 
+app = FastAPI()
 api_keys = ['AIzaSyBGoP2YHmhsZVXLjg3PCMaHTljykxA9Bls']
 
 current_key_index = 0
@@ -11,6 +15,7 @@ class QuotaExceededError(Exception):
     pass
 
 def call_api_keys(text, api_key):
+
 
     url = f"https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key={api_key}"
     data = {
@@ -53,9 +58,19 @@ def get_toxicity(text):
     raise Exception("All API keys quota exhausted")
 
 
-def flag_toxic_comment(thread, threshold):
+class ThreadRequest(BaseModel):
+    text: str  
+
+threshold = 0.6
+
+@app.post("/check_thread_toxicity/")
+
+def check_thread_toxicity(req: ThreadRequest):
+    
+    comments = [line.strip() for line in req.text.split('\n') if line.strip()]
+
     flagged_comments = []
-    for comment in thread:
+    for comment in comments:
 
         score = get_toxicity(comment)
         flagged = score >= threshold
@@ -65,15 +80,8 @@ def flag_toxic_comment(thread, threshold):
             'toxicity_score': score,
             'flagged_status': flagged
             })
-        
-    return flagged_comments
+   
+    return {"results": flagged_comments}
+
 
     
-
-
-if __name__ == "__main__":
-    thread = ["Hey nice to meet you", "you are not a good person", "you are such a baddie", "there are people worse than you", "you can never beat me ever", "I hate you", "you should just leave this earth", "just go and die, just kidding", "fuck you a million times", "Jiminy cricket! Well gosh durned it! Oh damn it all!","Rohan is a nigga", "I love you"]
-
-    results = flag_toxic_comment(thread, 0.6)
-    for result in results:
-        print(f"Comment : {result["comment"]}\nToxicity Score = {result["toxicity_score"]} \nFlagged : {result["flagged_status"]}")
